a3b23168bec79c02146b9b4208a15cd7e9b37fe4
mvelo8 <129048482+mvelo8@users.noreply.github.com>
2024-07-25 10:39:13 +0200

    Intial commit

---
+++ Customer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INDEXicetask
+{
+    
+    
+        public class CustomerIndex
+        {
+        decimal balance;
+        string email, phone, name;
+        DateTime lastPurchase;
+
+        public CustomerIndex(string name, string email, string phone, decimal balance, DateTime lastPurchase)
+            {
+                this.name = name;
+                this.email = email;
+                this.phone = phone;
+                this.balance = balance;
+                this.lastPurchase = lastPurchase;
+            }
+
+            public object this[int index]
+            {
+                get
+                {
+                    switch (index)
+                    {
+                        case 0:
+                            return this.name;
+                        case 1:
+                            return this.email;
+                        case 2:
+                            return this.phone;
+                        case 3:
+                            return this.balance;
+                        case 4:
+                            return this.lastPurchase;
+                        default:
+                            return null;
+                    }
+                }
+                set
+                {
+                    switch (index)
+                    {
+                        case 0:
+                            this.name = (string)value;
+                            break;
+                        case 1:
+                            this.email = (string)value;
+                            break;
+                        case 2:
+                            this.phone = (string)value;
+                            break;
+                        case 3:
+                            this.balance = (decimal)value;
+                            break;
+                        case 4:
+                            this.lastPurchase = (DateTime)value;
+                            break;
+                    }
+                }
+            }
+
+            public object this[string attrName]
+            {
+                get
+                {
+                    switch (attrName.ToLower())
+                    {
+                        case "name":
+                            return this.name;
+                        case "email":
+                            return this.email;
+                        case "phone":
+                            return this.phone;
+                        case "balance":
+                            return this.balance;
+                        case "lastpurchase":
+                            return this.lastPurchase;
+                        default:
+                            return null;
+                    }
+                }
+                set
+                {
+                    switch (attrName.ToLower())
+                    {
+                        case "name":
+                            this.name = (string)value;
+                            break;
+                        case "email":
+                            this.email = (string)value;
+                            break;
+                        case "phone":
+                            this.phone = (string)value;
+                            break;
+                        case "balance":
+                            this.balance = (decimal)value;
+                            break;
+                        case "lastpurchase":
+                            this.lastPurchase = (DateTime)value;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace INDEXicetask
+{
+    internal class Program
+    {
+     
+        
+            public static void Main(string[] args)
+            {
+                
+                CustomerIndex customer = new CustomerIndex("Mpimpi Hloni", "mpimpi12@gmail.com", "0589567894", 1000, new DateTime(2023, 6, 15));
+
+                
+                Console.WriteLine("Name: " + customer[0]);
+                Console.WriteLine("Email: " + customer["Email"]);
+                Console.WriteLine("Phone: " + customer[2]);
+                Console.WriteLine("Balance: " + customer["Balance"]);
+                Console.WriteLine("Last Purchase: " + customer["LastPurchase"]);
+
+                
+                customer[0] = "Zinhle Josh";
+                customer["Email"] = "zinhlejosh@gmail.com";
+                customer[2] = "0987654321";
+                customer["Balance"] = 1500;
+                customer["LastPurchase"] = new DateTime(2023, 8, 1);
+
+                
+                Console.WriteLine("\nUpdated Customer Information:");
+                Console.WriteLine("Name: " + customer[0]);
+                Console.WriteLine("Email: " + customer["Email"]);
+                Console.WriteLine("Phone: " + customer[2]);
+                Console.WriteLine("Balance: " + customer["Balance"]);
+                Console.WriteLine("Last Purchase: " + customer["LastPurchase"]);
+            }
+        }
+    }
+    
+
